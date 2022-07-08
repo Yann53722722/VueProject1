9f968706e201965417e1261cb3fe682ad9e40713
4 <MS_yjs@outlook.com>
2022-07-08 18:47:39 +0800

    feat: 头像&登出

---
+++ src/layout/headers/components/avatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      <el-avatar shape="square" :size="40" :src="squareUrl" />
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click="logout">退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+const squareUrl = ref(
+  'https://tse2-mm.cn.bing.net/th/id/OIP-C.RPSxv5isWsNFJ7SMoChHKwHaHa?w=159&h=180&c=7&r=0&o=5&pid=1.7'
+)
+const logout = () => {
+  store.dispatch('app/logout')
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-dropdown-menu__item {
+  white-space: nowrap;
+}
+</style>
